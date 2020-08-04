717d02a3bfcf201a6620f40855c249f1864a5a25
Alessandro L. Santos <aleluizsantos@gmail.com>
2020-08-04 19:04:02 -0300

    First Project

---
+++ web/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './assets/Styles/global.css';
+import Routes from './routes';
+
+
+
+
+function App() {
+  return (
+    <Routes />    
+  );
+}
+
+export default App;
---
+++ web/src/pages/Landing/index.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import giveClassesIcon from '../../assets/images/icons/give-classes.svg';
+import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
+import landingImg from '../../assets/images/landing.svg';
+import logoImg from '../../assets/images/logo.svg';
+import './styles.css';
+
+
+
+
+
+function Landing() {
+    return (
+        <div id="page-landing">
+            <div id="page-landing-content" className="container">
+                <div className="logo-container">
+                    <img src={logoImg} alt="Proffy" />
+                    <h2>Sua plataforma de estudo online.</h2>
+                </div>
+                <img 
+                    src={landingImg} 
+                    alt="Plataforma de estudo" 
+                    className="hero-image" 
+                />
+
+                <div className="buttoms-container">
+                    <Link to="/study" className="study">
+                        <img src={giveClassesIcon} alt="Estudar" />
+                        Estudar
+                    </Link>
+                    <Link to="give-classes" className="give-classes">
+                        <img src={giveClassesIcon} alt="Dar aulas" />
+                        Dar aulas
+                    </Link>
+                </div>
+                <span className="total-connections">
+                    Total de 200 conexões já realizados <img src={purpleHeartIcon} alt="Coração"/>
+                </span>
+            </div> 
+        </div>
+    );
+}
+
+export default Landing;
