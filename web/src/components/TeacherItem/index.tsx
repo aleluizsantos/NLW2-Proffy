38e0cc8cc65bc09addd5fabb51807d9d6e8ad067
Alessandro L. Santos <aleluizsantos@gmail.com>
2020-08-07 22:53:01 -0300

    finalizado_web

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -1,35 +1,55 @@
 import React from 'react';
+import api from '../../services/api';
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
 
-function TeacherItem() {
+export interface Teacher {
+        id: string;
+        avatar: string;
+        bio: string;
+        cost: Number;
+        name: string;
+        subject: string;
+        whatsapp: string;
+}
+
+interface TeacherItemProps {
+    teacher: Teacher
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({teacher}) => {
+
+    function createNewConnection() {
+        api.post('connections', {
+                user_id: teacher.id
+        });
+    }
+
     return (
         <article className="teacher-item">
             <header>
-                <img src="https://avatars3.githubusercontent.com/u/16269962?s=460&u=2ab727dfec7464c9f1059d66bc7581e7d60d9727&v=4" alt="Alessandro L Santos" />
+                <img src={teacher.avatar} alt={teacher.name} />
                 <div>
-                    <strong>Alessandro L. Santos</strong>
-                    <span>Química</span>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
                 </div>
             </header>
-            <p>
-                Entuaiasta das melhores tecnologias de química avaçada.
-                <br/><br/>
-                Apaixonado por explodir coisa em laboratório e por mudar a vida das pessoas
-                através de experiências.
-            </p>
+            <p>{teacher.bio}</p>
 
             <footer>
                 <p> 
                     Preço/hora 
-                    <strong>R$80,00</strong>
+                    <strong>R$ {teacher.cost}</strong>
                 </p>
-                <button type="button">
+                <a  target="_blank"
+                    onClick={createNewConnection} 
+                    href={`https://wa.me/${teacher.whatsapp}`}>
+
                     <img src={whatsappIcon} alt="whatsapp"/>
                     Entrar em contato
-                </button>
+                </a>
             </footer>
         </article>
     );
